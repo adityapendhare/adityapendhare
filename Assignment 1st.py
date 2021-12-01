bb1a2dad941be70899fb7a04fffc53100eeb185c
adityapendhare <95367453+adityapendhare@users.noreply.github.com>
2021-12-01 21:21:59 +0530

    Add files via upload

---
+++ Assignment 1st.py
@@ -0,0 +1,38 @@
+# Assigniong four values to the values as explained in he class
+a = 45
+b = 35
+c = 50
+d = 90
+
+#Performing arithmeic operations
+add = a + b + c + d
+print(a)
+print(b)
+print(c)
+print(d)
+print(add)
+print("*****************************")
+
+sub = a - b - c - d
+print(a)
+print(b)
+print(c)
+print(d)
+print(sub)
+print("*****************************")
+
+multi = a * b * c * d
+print(a)
+print(b)
+print(c)
+print(d)
+print(multi)
+print("*****************************")
+
+div = a / b / c / d
+print(a)
+print(b)
+print(c)
+print(d)
+print(div)
+print("*****************************")
